67df51b0c015dd97d97ba17809cde35394baafb7
IrreplaceL <lirreplace@gmail.com>
2024-10-23 19:05:23 +0800

    添加swagger

--- irreplace-blog/src/main/java/com/irreplace/job/UpdataViewCountJob.java
+++ irreplace-blog/src/main/java/com/irreplace/job/UpdataViewCountJob.java
@@ -25,7 +25,7 @@ public class UpdataViewCountJob {
     @Autowired
     private ArticleService articleService;
 
-    @Scheduled(cron = "0/55 * * * * ?")
+    @Scheduled(cron = "0 0/30 * * * ?")
     public  void updataViewCount(){
     //获取redis中的浏览量
         Map<String,Integer> viewCountMap = redisCache.getCacheMap("article:viewCount");
